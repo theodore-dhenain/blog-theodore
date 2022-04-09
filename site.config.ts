9674fe29b604d5c184c56a0b6d5c85abed771292
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2022-04-09 23:17:40 +0200

    Setup site config

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'a70ccafbe4d04cc7a3d02e4de3b97df1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Blog de Théodore Dhénain',
+  domain: 'theodore-dhenain.vercel.app',
+  author: 'Théodore Dhénain',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Mon blog personnel',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: null,
+  github: 'theodore-dhenain',
+  linkedin: 'th%C3%A9odore-dh%C3%A9nain-886447156',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
